33db6a1250be6606f508f11f50306a74df3b71be
DavidBachmann <davidbachmann@bitbucket.org>
2016-01-02 19:20:09 +0000

    Cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -96,6 +96,29 @@ window.addEventListener('load', function() {
     }
   })();
 
+  // Switch out About background-image on link hover
+  (function(){
+      var img = document.getElementById('js-right-img');
+      var link = document.getElementsByClassName("js-hover");
+      var imgUrl = '1';
+      var handleMouseOver = debounce(function() {
+      var self = this;
+
+      if (self.dataset.url) {
+        img.style.backgroundImage = "url('../images/" + self.dataset.url + ".jpg')";
+      }
+    }, 50);
+
+    var handleMouseOut = debounce(function() {
+      img.style.backgroundImage ="url('../images/" + imgUrl + ".jpg')";
+    }, 50);
+
+    for (var i = 0; i < link.length; i++) {
+      link[i].addEventListener('mouseover', handleMouseOver, true);
+      link[i].addEventListener('mouseout', handleMouseOut, true);
+    }
+  })();
+
   // Toggle menu
   (function(){
     var el = document.getElementById("js-toggle");
